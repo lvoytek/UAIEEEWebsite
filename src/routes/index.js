e9cdfca4cd85d51daa51b76ac1c5514e37fcb00b
Lena Voytek <dvoytek@email.arizona.edu>
2020-02-23 22:41:19 -0700

    Added file redirection

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile('index.html');
 });
+
+router.get('/about', function(req, res, next) {
+  res.sendFile('about.html');
+});
+
+router.get('/resources', function(req, res, next) {
+  res.sendFile('resources.html');
+});
+
+router.get('/senior_design', function(req, res, next) {
+  res.sendFile('senior_design.html');
+});
+
+router.get('/hours', function(req, res, next) {
+  res.sendFile('lab_hours.html');
+});
+
+router.get('/officers', function(req, res, next) {
+  res.sendFile('officers.html');
+});
+
+router.get('/live', function(req, res, next) {
+  res.sendFile('live_camera.html');
+});
+
+router.get('/calendar', function(req, res, next) {
+  res.sendFile('calendar.html');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.sendFile('signup.html');
+});
+
+
 
 module.exports = router;
